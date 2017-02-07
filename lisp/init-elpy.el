47b041d4f0fafcaad51fbc6044bbe29c613a4187
453992341@qq.com <453992341@qq.com>
2017-02-07 16:32:37 +0800

    change python mode interpreter shell to python3

--- lisp/init-elpy.el
+++ lisp/init-elpy.el
@@ -2,5 +2,6 @@
 5(elpy-enable)
 (elpy-use-ipython)
 (setq python-shell-interpreter "ipython"
-          python-shell-interpreter-args "--simple-prompt -i");;提示符乱码
+      python-shell-interpreter-args "--simple-prompt -i");;提示符乱码
+(elpy-use-cpython "/usr/local/bin/python3");;use python3
 (provide 'init-elpy)
